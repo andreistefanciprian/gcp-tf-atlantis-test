66ca2ebe2febf6dd6b5ffea06623f573e2c588ae
Stefan Andrei <andreistefanciprian@gmail.com>
2021-02-17 10:30:23 +0000

    Revert "update port dev"

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -26,7 +26,7 @@ resource "google_compute_firewall" "default" {
 
   allow {
     protocol = "tcp"
-    ports    = ["80", "8080", "1000-2000", "443"]
+    ports    = ["80", "8080", "1000-2000"]
   }
 
   source_tags = ["web"]
